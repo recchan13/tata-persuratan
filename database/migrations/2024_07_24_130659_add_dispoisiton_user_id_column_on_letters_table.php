0cdbdced5b9a58080dcdefba63690a56de7e341a
recchan13 <93rezao@gmail.com>
2024-07-24 12:12:38 +0700

    aa

---
+++ database/migrations/2024_07_24_130659_add_dispoisiton_user_id_column_on_letters_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('letters', function (Blueprint $table) {
+            $table->unsignedBigInteger('disposition_user_id');
+            $table->foreign('disposition_user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('letters', function (Blueprint $table) {
+            //
+        });
+    }
+};
