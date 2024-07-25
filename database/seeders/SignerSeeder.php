d5b8b107f8da9f431ce9d615ba8bcdfd5e0a95e5
recchan13 <93rezao@gmail.com>
2024-07-25 17:44:59 +0700

    seeder

---
+++ database/seeders/SignerSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Signer;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SignerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        Signer::create([
+            'code' => 'R',
+            'name' => 'Rektor',
+            'description' => 'Unntuk Nomor surat',
+        ]);
+    }
+}
